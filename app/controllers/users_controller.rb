a421dd0ebae4550554b8d8e417b610492d44ae9e
Allyson Wesman <allysonwesman@gmail.com>
2018-04-20 14:34:47 -0700

    add users controller methods and create user show view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,40 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+
+    if @user.nil?
+      flash[:notice] = "User not found"
+      redirect_to users_path
+    end
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+    def users_params
+      params.require(:user).permit(:name, :email) # other stuff??
+    end
+
 end
