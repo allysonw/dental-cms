b40a7e51f1c32403973e859f04b4416988789140
Allyson Wesman <allysonwesman@gmail.com>
2018-04-19 19:36:09 -0700

    write omniauth spec

--- Gemfile
+++ Gemfile
@@ -46,6 +46,7 @@ group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'rspec-rails', '~> 3.7'
   gem 'factory_bot_rails'
+  gem 'pry'
 end
 
 group :development do
@@ -64,7 +65,7 @@ group :test do
   gem 'selenium-webdriver'
   # Easy installation and use of chromedriver to run system tests with Chrome
   gem 'chromedriver-helper'
-  
+
   gem 'database_cleaner'
 end
 
--- spec/features/users/omniauth_spec.rb
+++ spec/features/users/omniauth_spec.rb
@@ -2,19 +2,24 @@ require 'rails_helper'
 
 RSpec.feature "Sign in", type: :devise do
   OmniAuth.config.test_mode = true
+
   OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new({
     :provider => 'google_oauth2',
     :uid => '123545',
-    :name => 'Allyson Wesman'
+    :info => {
+      :name => 'Andrew Wesman'
+    }
   })
-
 
   before do
     Rails.application.env_config["devise.mapping"] = Devise.mappings[:user] # If using Devise
     Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:google_oauth2]
   end
 
-  scenario "something" do
-    visit '/'
+  scenario "Sign in with Google account" do
+    visit new_user_session_path
+    expect(page).to have_content("Sign in with GoogleOauth2")
+    click_link "Sign in with GoogleOauth2"
+    expect(page).to have_content("Andrew Wesman")
   end
 end
