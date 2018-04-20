af0ddc8ac3b532ffb9a209f69f09f0538c409d46
Allyson Wesman <allysonwesman@gmail.com>
2018-04-20 14:05:59 -0700

    add show and index views and actions for appointments

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,2 +1,34 @@
 class AppointmentsController < ApplicationController
+  def index
+    @appointments = Appointment.all
+  end
+
+  def show
+    @appointment = Appointment.find_by(id: params[:id])
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+    def appointments_params
+      params.require(:appointment).permit(:date, :time, :location) # patient/user id??
+    end
 end
