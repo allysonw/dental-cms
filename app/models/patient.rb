a712e77777560f1be2d7317b559fb4a4244b8384
Allyson Wesman <allysonwesman@gmail.com>
2018-04-26 17:08:57 -0700

    destroy treatment plans if a pt is deleted

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -4,7 +4,7 @@ class Patient < ApplicationRecord
   # TODO currently all appointments for a patient are detroyed if the patient is deleted
   # Fix so a patient can be deleted but we can maintain their appointment records
 
-  has_many :treatment_plans
+  has_many :treatment_plans, dependent: :destroy
 
   validates :name, :dob, :address, :phone_number, presence: true
 end
