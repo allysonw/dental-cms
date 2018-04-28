4dec943c5cffc7006675186a9bc92fb8b4d91e2d
Allyson Wesman <allysonwesman@gmail.com>
2018-04-27 18:16:37 -0700

    test model changes for blog post. write blog post.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,4 @@ module ApplicationHelper
   def display_price(price)
     number_to_currency(price)
   end
-
-  def form_submit_button_text(object)
-    object.persisted? ? "Update #{object.model_name.human.titleize}" : "Create #{object.model_name.human.titleize}"
-  end
 end
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,8 +1,6 @@
 class Patient < ApplicationRecord
   has_many :appointments
   has_many :users, through: :appointments, dependent: :destroy
-  # TODO currently all appointments for a patient are detroyed if the patient is deleted
-  # Fix so a patient can be deleted but we can maintain their appointment records
 
   has_many :treatment_plans, dependent: :destroy
   has_one :address, dependent: :destroy
