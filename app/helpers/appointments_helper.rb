9893288b6869410d93871dc118cedc0a9c56326c
Allyson Wesman <allysonwesman@gmail.com>
2018-04-30 11:28:57 -0700

    add helper for showing appt notes on appt show page

--- app/helpers/appointments_helper.rb
+++ app/helpers/appointments_helper.rb
@@ -6,4 +6,11 @@ module AppointmentsHelper
   def appointment_confirm_text
      "Are you sure you would like to delete this appointment? This will also delete the appointment's notes!"
   end
+
+  def show_appointment_notes(appointment)
+    if !appointment.appointment_notes.blank?
+      render partial: 'appointment_notes/notes_table', locals: { appointment: appointment }
+    end
+  end
+
 end
