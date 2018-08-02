42baa7e23b21ab07404c87ae41cbf3c172359519
Allyson Wesman <allysonwesman@gmail.com>
2018-08-02 14:50:48 -0700

    add helper to return limited list of tooth numbers for new treatments

--- app/helpers/treatments_helper.rb
+++ app/helpers/treatments_helper.rb
@@ -11,4 +11,10 @@ module TreatmentsHelper
        render partial: "treatments/treatments_table", locals: { treatment_plan: treatment_plan, treatments: treatments }
     end
   end
+
+  def tooth_numbers
+    locations = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16']
+    locations.concat(['17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32'])
+    locations.concat(['UR', 'LR', 'UL', 'LL', 'UA', 'LA'])
+  end
 end
