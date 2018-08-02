f8ffcea4da99b36d6671fa70fdc35cc873158a3c
Allyson Wesman <allysonwesman@gmail.com>
2018-08-02 15:09:55 -0700

    style new treatment location selector

--- app/helpers/treatments_helper.rb
+++ app/helpers/treatments_helper.rb
@@ -15,6 +15,6 @@ module TreatmentsHelper
   def tooth_numbers
     locations = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16']
     locations.concat(['17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32'])
-    locations.concat(['UR', 'LR', 'UL', 'LL', 'UA', 'LA'])
+    locations.concat(['UR', 'LR', 'UL', 'LL', 'UA', 'LA', 'FM'])
   end
 end
